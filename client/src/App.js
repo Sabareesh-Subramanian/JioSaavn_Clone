62787606ad6bf55c7d9ab13362005766f0a5576b
Pulkit0111 <ptyagi1551@gmail.com>
2021-10-01 18:57:02 +0530

    Signup and login page with regidtration functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import { SignUp } from "./components/SignUp";
+import { LoginPage } from "./components/Login";
+import { MobileLogin } from "./components/MobileLogin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <SignUp />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/mobile">
+          <MobileLogin />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,98 @@
+import { Login } from "../stylemodules/LoginStyles";
+import signup from "../images/signup.png";
+import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [auth, setAuth] = useState(false);
+  const history = useHistory();
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePass = (e) => {
+    setPass(e.target.value);
+  };
+  const handleLogin = async () => {
+    let { data } = await axios.get("http://localhost:3001/users");
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].email === email && data[i].pass === pass) {
+        setEmail("");
+        setPass("");
+        setAuth(true);
+        break;
+      }
+    }
+    if (auth) {
+      history.push("/");
+    } else {
+      alert("wrong Credentials");
+    }
+  };
+  return (
+    <Login>
+      <div>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div>
+          <img src={signup} alt="signup_image" />
+        </div>
+        <div>
+          <p>All Your Music.</p>
+        </div>
+        <div>
+          <p>Anytime, anywhere</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Don't have a JioSaavn account yet?</p>
+          <button>
+            <Link to="/signup">
+              <span>Signup</span>
+            </Link>
+          </button>
+        </div>
+        <div>
+          <p>Welcome to JioSaavn.</p>
+          <p>Login with your email address.</p>
+        </div>
+        <div>
+          <input
+            onChange={handleEmail}
+            value={email}
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            onChange={handlePass}
+            value={pass}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={handleLogin}>Continue</button>
+        </div>
+        <div>
+          <p>
+            By selecting ‘Continue’, you agree to jioSaavn’s{" "}
+            <span>Terms of Service</span> and <span>Privacy Policy.</span>
+          </p>
+        </div>
+        <div>
+          <p>OR CONNECT WITH</p>
+        </div>
+        <div>
+          <Link to="/mobile">
+            <button>Mobile Number</button>
+          </Link>
+          <button>Facebook</button>
+        </div>
+      </div>
+    </Login>
+  );
+};
