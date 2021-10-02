cb70bdb8803fd5322aa68d153c97808a44707489
Mahipal Singh Haritwal <mahi98jat@gmail.com>
2021-10-02 17:09:44 +0530

    all the files related to search added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import { Homepage } from "./components/Homepage/Homepage";
+import { Route, Switch } from "react-router-dom";
+//import Search from "./components/SearchEngine/Search";
+import Player from "./components/Homepage/Player";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
     </div>
   );
 }
